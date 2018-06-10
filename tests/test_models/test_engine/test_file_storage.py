9ba1112147cb97087cf56182dd2de78d141918ef
gjdame <gregoryjdame@gmail.com>
2018-06-10 03:14:04 +0000

    add file storage testing

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import unittest
+import json
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+
+class TestFileStorage(unittest.TestCase):
+    '''testing file storage'''
+
+    @classmethod
+    def setUpClass(cls):
+        cls.rev1 = Review()
+        cls.rev1.place_id = "Raleigh"
+        cls.rev1.user_id = "Greg"
+        cls.rev1.text = "Grade A"
+
+    @classmethod
+    def teardown(cls):
+        del cls.rev1
+
+    def test_style_check(self):
+        """
+        Tests pep8 style
+        """
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_all(self):
+        pass
+
+    def test_new(self):
+        pass
+
+    def test_reload(self):
+
+    
