a63a5e6e4754b15f33ab70ebcb0d4304a283b4c3
gjdame <gregoryjdame@gmail.com>
2018-06-10 00:08:52 +0000

    add user and console functionality for all other classes

--- console.py
+++ console.py
@@ -5,14 +5,19 @@ Entry to command interpreter
 import cmd
 from models import storage
 from models.base_model import BaseModel
-
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """
     Entry to command interpreter
     """
     prompt = "(hbnb)"
-    classes = {"BaseModel", "State", "City", "Amenity", "Place", "Review"}
+    classes = {"BaseModel", "State", "City", "Amenity", "Place", "Review", "User"}
 
     def do_EOF(self, line):
         """Exit on Ctrl-D"""
