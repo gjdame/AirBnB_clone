7200fba48a3d9ffb56f4484cf0b557707a94c355
MelissaN <majajachiu@yahoo.com>
2018-06-11 00:14:43 +0000

    add tests for methods: reload, new, save

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Unittest to test FileStorage class
+"""
 import unittest
 import pep8
 import json
@@ -12,6 +14,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.engine.file_storage import FileStorage
+
 
 class TestFileStorage(unittest.TestCase):
     '''testing file storage'''
@@ -27,6 +30,12 @@ class TestFileStorage(unittest.TestCase):
     def teardown(cls):
         del cls.rev1
 
+    def teardown(self):
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
     def test_style_check(self):
         """
         Tests pep8 style
@@ -36,10 +45,41 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_all(self):
-        pass
+        """
+        Tests method: all (returns dictionary <class>.<id> : <obj instance>)
+        """
+        storage = FileStorage()
+        instances_dic = storage.all()
+        self.assertIsNotNone(instances_dic)
+        self.assertEqual(type(instances_dic), dict)
+        self.assertIs(instances_dic, storage._FileStorage__objects)
 
     def test_new(self):
-        pass
+        """
+        Tests method: new (saves new object into dictionary)
+        """
+        m_storage = FileStorage()
+        instances_dic = m_storage.all()
+        melissa = User()
+        melissa.id = 999999
+        melissa.name = "Melissa"
+        m_storage.new(melissa)
+        key = melissa.__class__.__name__ + "." + str(melissa.id)
+        #print(instances_dic[key])
+        self.assertIsNotNone(instances_dic[key])
 
     def test_reload(self):
-        pass
+        """
+        Tests method: reload (reloads objects from string file)
+        """
+        a_storage = FileStorage()
+        try:
+            os.remove("file.json")
+        except:
+            pass
+        with open("file.json", "w") as f:
+            f.write("{}")
+        with open("file.json", "r") as r:
+            for line in r:
+                self.assertEqual(line, "{}")
+        self.assertIs(a_storage.reload(), None)
