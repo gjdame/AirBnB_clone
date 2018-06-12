5fa81500b7d62a9178b58672134c3623e723146c
gjdame <gregoryjdame@gmail.com>
2018-06-12 19:54:50 +0000

    add check for obj existence in new filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,8 +25,9 @@ class FileStorage:
 
     def new(self, obj):
         '''Add new obj to existing dictionary of instances'''
-        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        if obj:
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         '''Save obj dictionaries to json file'''
